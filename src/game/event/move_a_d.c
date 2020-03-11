2a2321892cb7bfe2a3072f3764166cf5a84fa35a
Vmoreau <v.moreau96@hotmail.fr>
2020-03-11 17:52:46 +0100

    make sound when wall hited

--- src/game/event/move_a_d.c
+++ src/game/event/move_a_d.c
@@ -6,7 +6,7 @@
 /*   By: vmoreau <vmoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 12:04:21 by vmoreau           #+#    #+#             */
-/*   Updated: 2020/03/10 19:32:45 by vmoreau          ###   ########.fr       */
+/*   Updated: 2020/03/11 17:41:40 by vmoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static void	move_d(t_cub3d *cub)
 {
 	double spd;
+	static int time;
 
 	spd = MS + cub->spe.mov_spe;
 	if (cub->map.map[(int)(cub->map.pos_y + (cub->cast.plane.y_f * spd))]
@@ -23,11 +24,24 @@ static void	move_d(t_cub3d *cub)
 	if (cub->map.map[(int)cub->map.pos_y]
 				[(int)(cub->map.pos_x + (cub->cast.plane.x_f * spd))] == 0)
 		cub->map.pos_x += cub->cast.plane.x_f * spd;
+	if (cub->map.map[(int)(cub->map.pos_y + (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x + (cub->cast.plane.x_f * spd))] == 0)
+		time = 1;
+	if (cub->map.map[(int)(cub->map.pos_y + (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x + (cub->cast.plane.x_f * spd))] == 1 ||
+		cub->map.map[(int)(cub->map.pos_y + (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x + (cub->cast.plane.x_f * spd))] == 2)
+		if (time == 1)
+		{
+			ft_putchar('\a');
+			time = 0;
+		}
 }
 
 static void	move_a(t_cub3d *cub)
 {
 	double spd;
+	static int time;
 
 	spd = MS + cub->spe.mov_spe;
 	if (cub->map.map[(int)(cub->map.pos_y - (cub->cast.plane.y_f * spd))]
@@ -36,6 +50,18 @@ static void	move_a(t_cub3d *cub)
 	if (cub->map.map[(int)cub->map.pos_y]
 				[(int)(cub->map.pos_x - (cub->cast.plane.x_f * spd))] == 0)
 		cub->map.pos_x -= cub->cast.plane.x_f * spd;
+	if (cub->map.map[(int)(cub->map.pos_y - (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x - (cub->cast.plane.x_f * spd))] == 0)
+		time = 1;
+	if (cub->map.map[(int)(cub->map.pos_y - (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x - (cub->cast.plane.x_f * spd))] == 1 ||
+		cub->map.map[(int)(cub->map.pos_y - (cub->cast.plane.y_f * spd))]
+					[(int)(cub->map.pos_x - (cub->cast.plane.x_f * spd))] == 2)
+		if (time == 1)
+		{
+			ft_putchar('\a');
+			time = 0;
+		}
 }
 
 void		move_a_d(t_cub3d *cub, int keycode)
